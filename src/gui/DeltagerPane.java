2fd6865b483abb16d2aacfbb6ef391f6596e4fdc
Immer77 <peter_immersen@hotmail.com>
2022-04-22 12:00:30 +0200

    Tilføjet Labels og knapper til KonferencePane og Labels til DeltagerPane

--- src/gui/DeltagerPane.java
+++ src/gui/DeltagerPane.java
@@ -1,4 +1,62 @@
 package gui;
 
-public class DeltagerPane {
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import java.util.concurrent.Callable;
+
+
+public class DeltagerPane extends GridPane {
+    private TextField txfNavn, txfAdresse, txfby, txfLand, txfAnkomst, txfAfrejse, txfTlfNr, txfFirmaNavn, txfFirmaTlf, txfLedsagerNavn;
+
+
+
+    public DeltagerPane(){
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
+
+        Label lblnavn = new Label("Navn");
+        this.add(lblnavn,0,0);
+
+        Label lblAdrese = new Label("Adresse");
+        this.add(lblAdrese,0,1);
+
+        Label lblby = new Label("By");
+        this.add(lblby,0,2);
+
+        Label lblLand = new Label("Land");
+        this.add(lblLand,0,3);
+
+        Label lblAnkomst = new Label("Ankomstdato");
+        this.add(lblAnkomst,0,4);
+
+        Label lblAfrejse = new Label("Afrejsedato");
+        this.add(lblAfrejse,0,5);
+
+        Label lblForedragsHolder = new Label("Foredragsholder ja/nej");
+        this.add(lblForedragsHolder,2,0);
+
+        Label lbltlfNr = new Label("Tlf. Nr.");
+        this.add(lbltlfNr,2,1);
+
+        Label lblFirmaNavn = new Label("Firma navn");
+        this.add(lblFirmaNavn,2,2);
+
+        Label lblFirmaTlf = new Label("Firma Tlf.");
+        this.add(lblFirmaTlf,2,3);
+
+        //TODO: Indsæt tekstfield til Labels
+
+        //TODO: Indsæt CheckBokse til foredragsholder, Ledsager, arrangement, hotel og tillæg
+
+        //TODO: Tilføj radiobutton med VBOX Hotel
+
+
+    }
+
+    //TODO Tilføj opretTilmeldingsMetode til knappen OpretTilmelding
 }
--- src/gui/KonferencePane.java
+++ src/gui/KonferencePane.java
@@ -1,4 +1,99 @@
 package gui;
 
-public class KonferencePane {
+
+import controller.Controller;
+import javafx.beans.value.ChangeListener;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import modelclass.Arrangement;
+import modelclass.Deltager;
+import modelclass.Hotel;
+import modelclass.Konference;
+
+
+public class KonferencePane extends GridPane {
+    private ListView<Konference> lvwKonference;
+    private ListView<Hotel> lvwHoteller;
+    private ListView<Arrangement> lvwArrangement;
+    private ListView<Deltager> lvwDeltagere;
+
+
+
+    public KonferencePane(){
+        this.setPadding(new Insets(20));
+        this.setHgap(20);
+        this.setVgap(10);
+        this.setGridLinesVisible(false);
+
+        Label lblKonference = new Label("Konferencer");
+        this.add(lblKonference,0,0);
+
+        lvwKonference = new ListView<>();
+        this.add(lvwKonference,0,1,1,3);
+        lvwKonference.setPrefHeight(100);
+        lvwKonference.setPrefWidth(200);
+        lvwKonference.getItems().setAll(Controller.getKonferencer());
+
+        Label lblHotel = new Label("Hoteller");
+        this.add(lblHotel,0,4);
+
+        lvwHoteller = new ListView<>();
+        this.add(lvwHoteller,0,5,1,3);
+        lvwHoteller.setPrefWidth(200);
+        lvwHoteller.setPrefHeight(100);
+        lvwHoteller.getItems().setAll(Controller.gethoteller());
+
+        Label lblArrangement = new Label("Arrangement");
+        this.add(lblArrangement,2,0);
+
+        lvwArrangement = new ListView<>();
+        this.add(lvwArrangement,2,1,1,3);
+        lvwArrangement.setPrefHeight(100);
+        lvwArrangement.setPrefWidth(200);
+        lvwArrangement.getItems().setAll(Controller.getarrangementer());
+
+        Label lblDeltager = new Label("Deltager");
+        this.add(lblDeltager,2,4);
+
+        lvwDeltagere = new ListView<>();
+        this.add(lvwDeltagere,2,5,1,3);
+        lvwDeltagere.setPrefWidth(200);
+        lvwDeltagere.setPrefHeight(100);
+        lvwDeltagere.getItems().setAll(Controller.getdeltagere());
+
+        HBox hBoxButtons = new HBox();
+        this.add(hBoxButtons,1,9);
+        hBoxButtons.setPadding(new Insets(10,0,0,0));
+        hBoxButtons.setAlignment(Pos.BASELINE_CENTER);
+
+        Button btnOpretKonference = new Button("Opret Konference");
+        hBoxButtons.getChildren().add(btnOpretKonference);
+        btnOpretKonference.setOnAction(event -> this.opretKonferenceAction());
+
+        Button btnOpretArrangement = new Button("Opret Arrangement");
+        hBoxButtons.getChildren().add(btnOpretArrangement);
+        btnOpretArrangement.setOnAction(event -> this.opretArrangementAction());
+
+        Button btnOpretHotel = new Button("Opret Hotel");
+        hBoxButtons.getChildren().add(btnOpretHotel);
+        btnOpretHotel.setOnAction(event -> this.opretHotelAction());
+
+    }
+
+    private void opretHotelAction() {
+        //TODO
+    }
+
+    private void opretArrangementAction() {
+        //TODO
+    }
+
+    private void opretKonferenceAction() {
+        //TODO
+    }
 }
